56c1423a137b17624d722d7500e7e45a10d3e16d
loulou733 <loevan733@gmail.com>
2026-05-06 14:28:50 +0200

    Changement des chemin sur le setup

--- setup.ps1
+++ setup.ps1
@@ -6,10 +6,10 @@ Write-Host "==> Vérification de Python."
 python --version
 
 Write-Host "`n==> Installation des dépendances."
-pip install -r database/requirements.txt
+pip install -r projet_etude_agriculture_arda_loevan_lyliane_benoit/database/requirements.txt
 
 Write-Host "`n==> Création du schéma et import des CSV."
-python database/init_db.py
+python projet_etude_agriculture_arda_loevan_lyliane_benoit/database/init_db.py
 
 Write-Host "`nInstallation terminée." -ForegroundColor Green
-Write-Host "Lance l'API avec : python database/api.py"
+Write-Host "Lance l'API avec : python projet_etude_agriculture_arda_loevan_lyliane_benoit/database/api.py"
